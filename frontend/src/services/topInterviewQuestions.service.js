e787d7fe52adc8afcd59b87918f783c2ec0c1d6f
Saketh Ram Pusuluru <saketh.pusuluru@sasi.ac.in>
2026-03-17 11:51:26 +0530

    fixed api issue

--- frontend/src/services/topInterviewQuestions.service.js
+++ frontend/src/services/topInterviewQuestions.service.js
@@ -1,4 +1,4 @@
-import API_URL from "@/src/lib/api";
+import API_URL from "./api.js";
 
 export const generateTopInterviewQuestions = async ({ role, experience }) => {
   const res = await API_URL.post(
